e950306f4dd9af0499353bd61d7558f2a80f3466
AliShan <nxb.alishan@gmail.com>
2018-05-21 15:41:33 +0500

    basic modules and testing

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,55 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  items = [];
+  is_toast_shown: boolean;
 
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+      for (let index = 0; index < 20; index++) {
+        
+        this.items.push(this.items.length)
+        
+      }
   }
+
+  doInfinite(infiniteScroll) { 
+
+    setTimeout(() => {
+
+      if (this.items.length > 40){
+        this.FinishInfinite(infiniteScroll);
+      }else {
+        for (let i = 0; i <= 10; i++) {
+
+          if (this.items.length > 40){
+            this.FinishInfinite(infiniteScroll);
+          } 
+          this.items.push( this.items.length );
+        } 
+        infiniteScroll.complete();
+      } 
+       
+     
+    }, 500);
+  }
+
+  FinishInfinite(infiniteScroll){
+    infiniteScroll.complete();
+    this.ListFinishToast();
+    this.is_toast_shown = true;
+  }
+
+  ListFinishToast() {
+
+    if (this.is_toast_shown)
+      return;
+
+    let toast = this.toastCtrl.create({
+      message: 'No more data.',
+      duration: 5000
+    });
+    toast.present();
+  }
+
 
 }
